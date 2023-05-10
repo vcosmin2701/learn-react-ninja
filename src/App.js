d2944d77a7b7e445cebba58af7867e61d428ffad
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-05-10 15:31:35 +0300

    Working with multiple components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-  const linkGoogle = "https://google.com";
-
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>{ title }</h1>
-        <p>Liked {likes} times </p>
-        <p>{ [1,2,3,4,5]}</p>
-        <p>{ Math.random()*10}</p>
-        <a href={linkGoogle}>Google site but dynamic</a>
+        <Home />
       </div>
     </div>
   );
