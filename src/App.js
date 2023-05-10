7ff14c9edd8bd34435e7cb9e7f073ac7cae648da
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-05-10 15:20:33 +0300

    Dynamic values in Templates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  const linkGoogle = "https://google.com";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{ title }</h1>
+        <p>Liked {likes} times </p>
+        <p>{ [1,2,3,4,5]}</p>
+        <p>{ Math.random()*10}</p>
+        <a href={linkGoogle}>Google site but dynamic</a>
+      </div>
     </div>
   );
 }
