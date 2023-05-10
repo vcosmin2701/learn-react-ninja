88d67e4960b4036a0a861ae3782bf94724c0365b
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-05-10 23:48:51 +0300

    Reusing components. Filter JS

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import BlogList from "./BlogList";
 
 const Home = () => {
     const [blogs,setBlogs] = useState([
-        {title: 'My new website', body:'lorem ipsum...', author: 'mario', id:1},
+        {title: 'My new website', body:'lorem ipsum...', author: 'cosmo', id:1},
         {title: 'ITP Accepted Internship Journey', body:'lorem ipsum...', author: 'mili', id:2},
         {title: 'ITP Accepted Internship Journey', body:'lorem ipsum...', author: 'cosmo', id:3}
     ]);
@@ -11,6 +11,7 @@ const Home = () => {
     return (  
         <div className="home">
             <BlogList blogs={blogs} title="All blogs!"/>
+            <BlogList blogs={blogs.filter((blog) => blog.author === 'cosmo')} title="Cosmo's blogs"/>
         </div>
     );
 }
