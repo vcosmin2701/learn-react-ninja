2476c77e96a1593b9e48d31a843e74d8b4c1eb65
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-05-11 00:07:13 +0300

    Functions as props

--- src/Home.js
+++ src/Home.js
@@ -8,10 +8,14 @@ const Home = () => {
         {title: 'ITP Accepted Internship Journey', body:'lorem ipsum...', author: 'cosmo', id:3}
     ]);
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
     return (  
         <div className="home">
-            <BlogList blogs={blogs} title="All blogs!"/>
-            <BlogList blogs={blogs.filter((blog) => blog.author === 'cosmo')} title="Cosmo's blogs"/>
+            <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete}/>
         </div>
     );
 }
