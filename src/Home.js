b0bde452d9e154b9deb2bb8b956113af11ae26c4
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-05-10 16:12:15 +0300

    Click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+    const handleClick = (e) =>{
+        console.log('Hello Cosmos', e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name, e.target);
+    }
     return (  
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => {
+                handleClickAgain('Cosmin',e);
+            }}>Click me again</button>
         </div>
     );
 }
