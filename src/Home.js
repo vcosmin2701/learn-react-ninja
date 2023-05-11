2c81f164aaca6326a5e1ece9ea1628e1bd786d84
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-05-11 09:55:23 +0300

    useEffect basics

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -13,8 +13,13 @@ const Home = () => {
         setBlogs(newBlogs);
     }
 
+    useEffect(() =>{
+        console.log('use effect ran');
+        console.log(blogs);
+    });
+
     return (  
-        <div className="home">
+        <div className="home"> 
             <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete}/>
         </div>
     );
