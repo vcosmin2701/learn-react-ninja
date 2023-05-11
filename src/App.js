16751fd7c940018b12ca0eb73db2dc13126b6ac8
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-05-11 12:36:08 +0300

    Router params

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import Navbar from './Navbar';
 import Home from './Home';
-import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/create' element={<Create />} />
+            <Route path='/blogs/:id' element={<BlogDetails />} />
           </Routes>
         </div>
       </div>
--- src/BlogList.js
+++ src/BlogList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({blogs, title, handleDelete}) => {
     return (
         <div className="blog-list">
@@ -5,8 +7,10 @@ const BlogList = ({blogs, title, handleDelete}) => {
             {
                 blogs.map((blog) => (
                     <div className="blog-preview">
-                        <h2>{blog.title}</h2>
-                        <p>Written by {blog.author}</p>
+                        <Link to={`/blogs/${blog.id}`}>
+                            <h2>{blog.title}</h2>
+                            <p>Written by {blog.author}</p>
+                        </Link>
                     </div>
                 ))
             } 
