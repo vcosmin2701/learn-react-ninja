764ca872bf20b0f5fae935157e649772a6aa092c
vcosmin2701 <cosmin.vladut@ulbsibiu.ro>
2023-05-11 10:52:57 +0300

    Handling errors

--- src/Home.js
+++ src/Home.js
@@ -4,22 +4,34 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs,setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() =>{
         setTimeout(()=>{
-            fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json();
-            })
-            .then((data) => {
-                setBlogs(data);
-                setIsPending(false);
-            });
+                fetch('http://localhost:8000/blogs')
+                .then(res => {
+                    if(!res.ok){
+                        throw Error('could not fetch data for that resource');
+                    }
+                    return res.json();
+                })
+                .then((data) => {
+                    setBlogs(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setIsPending(false);
+                    setError(err.message);
+                })
         }, 1000);
     }, []);
 
     return (  
         <div className="home"> 
+            {
+                error && <div>{error}</div>
+            }
             {
                 isPending && <div>Loading...</div>
             }
